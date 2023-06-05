bc5812d03ac4e357389c7cad667458dabc1979dc
Anuraag001 <anuraagbv1@gmail.com>
2023-06-05 15:09:51 +0530

    Participant sign up page navigation and admin portal

--- Eventizer/urls.py
+++ Eventizer/urls.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-
 from User.views import *
 from Organiser_Main.views import *
-
+from Participant_Main.views import *
 
 urlpatterns = [
-    path('', Login, name="User_Login"),
-    path('signup', Signup, name="User_signup"),
+    #path('', Login, name="User_Login"),
+    path('', Signup, name="User_signup"),
     path('organiser_signup/', Organiser_signup, name='Organiser_signup'),
     path('participant_signup/', Participant_signup, name='Participant_signup'),
-    
     path('Organiser',organiser_main,name="organiser_main"),
-    
+    path('Participant',participant_main,name="participant_main"),
     path('admin/', admin.site.urls),
 ]
--- Participant_Main/views.py
+++ Participant_Main/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def participant_main(request):
+    return render(request,'par_index.html')
